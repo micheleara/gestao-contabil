47941cc058872adc61bb74ba83a32bfb8db3a6f3
Michele Oliveira <shellyguns@gmail.com>
2026-03-15 13:48:18 -0300

    feat: projeto inicial gestao-contabil

---
+++ src/main/java/br/com/banco/gestao_contabil/GestaoContabilApplication.java
@@ -0,0 +1,13 @@
+package br.com.banco.gestao_contabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoContabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoContabilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/banco/gestao_contabil/GestaoContabilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.banco.gestao_contabil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoContabilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
